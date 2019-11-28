8b87de506f7a7109ef9bcc6d592148f51ad0a053
wangjie-115 <wangjie-115@cpic.com.cn>
2019-11-28 12:56:17 +0800

    项目初始化

---
+++ demo-eureka/src/main/java/com/wj/cloud/demoeureka/DemoEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.wj.cloud.demoeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaApplication.class, args);
+    }
+
+}
